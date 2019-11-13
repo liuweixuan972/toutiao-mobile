fd4b117d5f19163ed8abacf53dddb59e2c4b77bb
liuweixuan123 <97235102@qq.com>
2019-11-13 20:31:45 +0800

    初始化
    git commit -m初始化
    git commit -m初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'amfe-flexible'
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
